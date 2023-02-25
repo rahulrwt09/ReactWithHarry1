b7ad1fe134255b850366dda7dadc868e3eb046ae
rahulrwt09 <golubook09@gmail.com>
2023-02-25 08:54:00 +0000

    add  select all and copy button

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -27,6 +27,10 @@ export default function Textform(props) {
       console.log(newtext);
       setText(newtext);
     };
+    const handleclick5= ()=>{
+      navigator.clipboard.writeText(text);
+      alert("COPY SUCESFULLY")
+         }
     const handleonchange=(event)=>{
        console.log("onchange");
        setText(event.target.value);
@@ -40,10 +44,12 @@ export default function Textform(props) {
       <div className="mb-3">
  
   <textarea className="form-control" onChange={handleonchange} value={text} id="mybox" rows="8"></textarea>
-  <button className='btn btn-primary my-4' onClick={handleclick1}>Convert to upper case</button>
-  <button className='btn btn-primary my-4 mx-4' onClick={handleclick2}>Convert to lower case</button>
-  <button className='btn btn-primary my-4 mx-4' onClick={handleclick3}>Clear Text</button>
-  <button className='btn btn-primary my-4 mx-4' onClick={handleclick4}>Select all Text</button>
+  <button className='btn btn-primary my-4 mx-2' onClick={handleclick1}>Convert to upper case</button>
+  <button className='btn btn-primary my-4 mx-2' onClick={handleclick2}>Convert to lower case</button>
+  
+  <button className='btn btn-primary my-4 mx-2' onClick={handleclick4}>Select All Text</button>
+  <button className='btn btn-primary my-4 mx-2' onClick={handleclick5}>Copy All</button>
+  <button className='btn btn-primary my-4 mx-2 bg-danger' onClick={handleclick3}>Clear Text</button>
 </div>
 
 <div className="container my-3">
